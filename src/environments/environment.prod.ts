572a3029f2d4d6536b32f697030f3f44e0ac59e2
Abderrahmane Arbaoui <a.arbaoui184@gmail.com>
2021-01-29 19:57:43 +0100

    add firebase config and adjust version

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyB2_Tdi3n1ZWDIr0UXzp9Ewo_yehArYqEU',
+    authDomain: 'catchmeon-4e321.firebaseapp.com',
+    databaseURL: 'https://catchmeon-4e321-default-rtdb.europe-west1.firebasedatabase.app',
+    projectId: 'catchmeon-4e321',
+    storageBucket: 'catchmeon-4e321.appspot.com',
+    messagingSenderId: '576104472842',
+    appId: '1:576104472842:web:2b6192af53a28f0559daf3',
+    measurementId: 'G-EJD4DEM01G'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyB2_Tdi3n1ZWDIr0UXzp9Ewo_yehArYqEU',
+    authDomain: 'catchmeon-4e321.firebaseapp.com',
+    databaseURL: 'https://catchmeon-4e321-default-rtdb.europe-west1.firebasedatabase.app',
+    projectId: 'catchmeon-4e321',
+    storageBucket: 'catchmeon-4e321.appspot.com',
+    messagingSenderId: '576104472842',
+    appId: '1:576104472842:web:2b6192af53a28f0559daf3',
+    measurementId: 'G-EJD4DEM01G'
+  }
 };
 
 /*
