bff66088ecbd1d5c8c615a27a159f71463505a80
Abderrahmane Arbaoui <a.arbaoui184@gmail.com>
2021-02-06 20:39:30 +0100

    fix auth guard blockage

--- src/app/shared/guard/auth.guard.ts
+++ src/app/shared/guard/auth.guard.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
 import { AuthService } from '../services/auth.service';
 import { Observable } from 'rxjs';
+import {AngularFireAuth} from '@angular/fire/auth';
+import {map} from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -11,16 +13,15 @@ export class AuthGuard implements CanActivate {
 
   constructor(
     public authService: AuthService,
-    public router: Router
+    public router: Router,
+    public afAuth: AngularFireAuth
   ){ }
 
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
-    if(this.authService.isLoggedIn !== true) {
-      this.router.navigate(['sign-in'])
-    }
-    return true;
+
+    return this.afAuth.authState.pipe(map(user => !!user))
   }
 
 }
